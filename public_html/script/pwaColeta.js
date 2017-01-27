ac4156f0c0c7810e23d1551e1a6e01ad9a9a43df
Wellington Gonçalves Pires <wellington.goncalves@live.com>
2017-01-27 19:10:39 -0200

    PWAColeta Adicionando e melhorando o cronometro

--- public_html/script/pwaColeta.js
+++ public_html/script/pwaColeta.js
@@ -8,10 +8,6 @@ var components = function () {
         return $('#btnNovaLinha');
     };
 
-    var btnDelLinha = function () {
-        return $('#btnDelLinha');
-    };
-
     var txtHoraAtual = function () {
         return $('#txtHoraAtual');
     };
@@ -20,29 +16,47 @@ var components = function () {
         return $('.checkboxesClass');
     };
 
+    var tblColeta = function () {
+        return $('#tblColeta');
+    };
+
+    var btnIniciar = function () {
+        return $('#btnIniciar');
+    };
+
+    var btnPausar = function () {
+        return $('#btnPausar');
+    };
+
+    var btnParar = function () {
+        return $('#btnParar');
+    };
+
     return{
         divFuncInfo: func_info,
         btnNovaLinha: btnNovaLinha,
-        btnDelLinha: btnDelLinha,
         txtHoraAtual: txtHoraAtual,
-        idCheckbox: idCheckbox
+        idCheckbox: idCheckbox,
+        tblColeta: tblColeta,
+        btnIniciar: btnIniciar,
+        btnPausar: btnPausar,
+        btnParar: btnParar
     };
 
 }();
 
-var ids = function () {
-
-
-};
-
 window.onload = function () {
+    var TEXTO_INICIAR = 'INICIAR';
+    var TEXTO_PAUSAR = 'PAUSAR';
+
+
+
     startTime();
     components.btnNovaLinha().click(function () {
         var rowCount = $('#tblColeta > tbody > tr').length;
 
         if (rowCount === 1) {
-            components.btnDelLinha().attr("disabled", true);
-            components.btnDelLinha().attr("disabled", false);
+            components.btnIniciar().attr("disabled", false);
         }
 
         if (rowCount > 5) {
@@ -53,39 +67,89 @@ window.onload = function () {
             $('#tblColeta > tbody:last-child').append(data);
             var randomId = parseInt(Math.random() * 1000);
             var divId = 'idDiv' + randomId;
-            var playId = 'imgPlay' + randomId;
+            var idBtnAcao = 'btnAcao' + randomId;
             var tempoId = 'spanTempo' + randomId;
+            var deletarId = 'btnDeletar' + randomId;
 
             $('#id_div_inicial').attr('id', divId);
-            $('#id_play_inicial').attr('id', playId);
+            $('#id_btn_inicial').attr('id', idBtnAcao);
+            $('#id_btn_apagar').attr('id', deletarId);
             $('#spanTempo').attr('id', tempoId);
 
+            var jqueryBtnAcao = '#' + idBtnAcao;
+            $(jqueryBtnAcao).click(function (e) {
+                $(jqueryBtnAcao).attr('value', 'REGISTRAR');
+                var rowIndex = $('#tblColeta tr').index(e.target.parentNode.parentNode);
+                var componentDisplay = undefined;
+                var spanComponent = $('#tblColeta tr span');
+                var buttonComponent = $('#tblColeta tr td button');
+                if ($('table tr span')[rowIndex] === undefined) {
+                    componentDisplay = spanComponent[1];
+                } else {
+                    componentDisplay = spanComponent[rowIndex];
+                }
 
-            $('table tr img').on('click', function (e) {
+                if (buttonComponent[rowIndex] === undefined) {
+                    buttonComponent[0].disabled = false;
+                    buttonComponent[rowIndex - 1].disabled = true;
+                } else {
+                    buttonComponent[rowIndex].disabled = false;
+                    buttonComponent[rowIndex - 1].disabled = true;
+                }
+                
+                if(componentDisplay.innerHTML === '05:00') {
+                    CountDown().Start(300000, componentDisplay);
+                }
 
             });
 
-
+            var jqueryBtnDeletar = '#' + deletarId;
+            $(jqueryBtnDeletar).click(removerLinha);
 
         });
     });
 
-    components.btnDelLinha().click(function () {
-        $('input:checked').each(function () {
-            $(this).parents('tr').remove();
+    components.btnIniciar().click(function () {
+        if (!confirm('Tem certeza?')) {
+            return;
+        }
 
-            var rowCount = $('#tblColeta > tbody > tr').length;
-            if (rowCount === 1) {
-                components.btnDelLinha().attr('disabled', true);
-                components.btnNovaLinha().attr("disabled", false);
+        if (components.btnIniciar().val() === TEXTO_INICIAR) {
+            components.btnIniciar().attr('value', TEXTO_PAUSAR);
+            components.btnParar().attr("disabled", false);
+            $('#tblColeta tr td button')[0].disabled = false;
+
+            CountDown().Start(70000, $('#tblColeta tr td span')[0]);
+
+        } else if (components.btnIniciar().val() === TEXTO_PAUSAR) {
+            components.btnIniciar().attr('value', TEXTO_INICIAR);
+            $('#tblColeta tr td button').attr('disabled',true);
+            var id = setTimeout(function(){}, 0);
+            while(id--){
+                clearTimeout(id);
             }
-            if (rowCount < 6) {
-                components.btnNovaLinha().attr("disabled", false);
-            }
-        });
+            $('#tblColeta tr td span').each(function(index, comp){
+                comp.innerHTML = '05:00';
+            });
+            
+        }
+
     });
-
 };
+
+function removerLinha() {
+    $(this).parents('tr').remove();
+
+    var rowCount = $('#tblColeta > tbody > tr').length;
+    if (rowCount === 1) {
+        components.btnIniciar().attr("disabled", true);
+        components.btnNovaLinha().attr("disabled", false);
+    }
+    if (rowCount < 6) {
+        components.btnNovaLinha().attr("disabled", false);
+    }
+}
+
 function startTime() {
     var today = new Date();
     var h = today.getHours();
@@ -94,8 +158,9 @@ function startTime() {
     m = checkTime(m);
     s = checkTime(s);
     components.txtHoraAtual().text(h + ":" + m + ":" + s);
-    var t = setTimeout(startTime, 500);
+    var t = setTimeout(startTime, 0);
 }
+
 function checkTime(i) {
     if (i < 10) {
         i = "0" + i;
@@ -104,29 +169,97 @@ function checkTime(i) {
     return i;
 }
 
-function startTimer(duration, display) {
-    var timer = duration, minutes, seconds;
-    setInterval(function () {
-        minutes = parseInt(timer / 60, 10);
-        seconds = parseInt(timer % 60, 10);
+var timeout;
 
-        minutes = minutes < 10 ? "0" + minutes : minutes;
-        seconds = seconds < 10 ? "0" + seconds : seconds;
+var CountDown = function () {
 
-        display.text(minutes + ":" + seconds);
+    var firstTime = false;
+    // Length ms 
+    var TimeOut = 10;
+    // Interval ms
+    var TimeGap = 1000;
 
-        if (--timer < 0) {
-            timer = duration;
+    var CurrentTime = (new Date()).getTime();
+    var EndTime = (new Date()).getTime() + TimeOut;
+
+    var GuiTimer = null;
+
+    var Running = false;
+
+    var UpdateTimer = function () {
+        // Run till timeout
+        if (CurrentTime + TimeGap < EndTime) {
+            timeout = setTimeout(UpdateTimer, TimeGap);
         }
-    }, 1000);
-}
+        // Countdown if running
+        if (Running) {
+            CurrentTime += TimeGap;
+            if (GuiTimer.innerHTML === '01:00') {
+                GuiTimer.parentNode.parentNode.style.backgroundColor = 'red';
+            }
+        }
+        // Update Gui
+        var Time = new Date();
+        Time.setTime(EndTime - CurrentTime);
+        var Minutes = Time.getMinutes();
+        var Seconds = Time.getSeconds();
 
-jQuery(function ($) {
-    var fiveMinutes = 60 * 5;
+        GuiTimer.innerHTML = (Minutes < 10 ? '0' : '') + Minutes + ':' + (Seconds < 10 ? '0' : '') + Seconds;
+    };
 
-    var rowIndex = $('table tr img').closest('td').parent()[0].sectionRowIndex;
-    $('table tr span').closest('td').parent()[rowIndex].children[5].children[0].innerHTML = Math.random() * 1000;
+    var Pause = function () {
+        Running = false;
+    };
 
-    var display = $('#time');
-    startTimer(fiveMinutes, display);
-});
+    var Resume = function () {
+        Running = true;
+    };
+
+    var Start = function (Timeout, componentDisplay) {
+        Running = true;
+        if (!firstTime) {
+            GuiTimer = componentDisplay;
+            TimeOut = Timeout;
+            CurrentTime = (new Date()).getTime();
+            EndTime = (new Date()).getTime() + TimeOut;
+            firstTime = true;
+            UpdateTimer();
+        }
+    };
+
+    return {
+        Pause: Pause,
+        Resume: Resume,
+        Start: Start
+    };
+};
+
+
+//var cronometro = undefined;
+//function startTimer(duration, display) {
+//    var timer = duration;
+//    var minutes;
+//    var seconds;
+//    cronometro = setInterval(function () {
+//        minutes = parseInt(timer / 60, 10);
+//        seconds = parseInt(timer % 60, 10);
+//
+//        minutes = minutes < 10 ? "0" + minutes : minutes;
+//        seconds = seconds < 10 ? "0" + seconds : seconds;
+//
+//        if (display !== undefined) {
+//            display.innerHTML = minutes + ":" + seconds;
+//        }
+//
+//        if (--timer < 0) {
+//            timer = duration;
+//        }
+//    }, 1000);
+//}
+//
+//function iniciarTimer(display) {
+//    var fiveMinutes = 5;
+//    var minutos = 60 * fiveMinutes;
+//    startTimer(minutos, display);
+//}
+//;
